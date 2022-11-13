06bf7a54f54b798beced9edf58a220e3e45b8b3c
Joshua Melvin <joshuamlvn@gmail.com>
2022-11-13 16:07:15 +0700

    Generate Employee list, detail, and add page

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    EmployeeDetailComponent
+  ],
   imports: [
     CommonModule,
     EmployeeRoutingModule
