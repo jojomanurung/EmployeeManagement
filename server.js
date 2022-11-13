6845f79a577efc3359b23a1b5b1af442fbb3df8a
Joshua Melvin <joshuamlvn@gmail.com>
2022-11-13 21:47:06 +0700

    Update approach instead save token in localStorage to use cookies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const jsonServer = require("json-server");
+const cookieParser = require("cookie-parser");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "./db/db.json"));
@@ -33,9 +34,9 @@ function verifyToken(token) {
 }
 
 /**
- * Fungsi untuk melakukan pengecekan akses login
+ * Fungsi untuk melakukan pengecekan employee ada di DB
  */
-function isAuthenticated({ username, password }) {
+function isEmployee({ username, password }) {
   const foundEmployee = dbEmployee.employee.find(
     (employee) =>
       employee.username === username && employee.password === password
@@ -43,14 +44,35 @@ function isAuthenticated({ username, password }) {
   return foundEmployee;
 }
 
-server.use(middlewares);
+/**
+ * middleware untuk request authenticated atau tidak
+ */
+function isAuthenticated(req, res, next) {
+  const token = req.cookies.access_token;
+  if (!token) {
+    const status = 401;
+    const message = "Authorization header is missing";
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    verifyToken(token);
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = "Error: token is invalid";
+    res.status(status).json({ status, message });
+  }
+}
 
+server.use(middlewares);
+server.use(cookieParser());
 server.use(jsonServer.bodyParser);
 
 // Post Method untuk api login
 server.post("/auth/login", (req, res) => {
   const { username, password } = req.body;
-  const user = isAuthenticated({ username, password });
+  const user = isEmployee({ username, password });
 
   if (!user) {
     const status = 401;
@@ -59,24 +81,25 @@ server.post("/auth/login", (req, res) => {
     return;
   }
   delete user.password;
-  const access_token = createToken({ username, password });
-  res.status(200).json({ user, access_token });
+  const access_token = createToken(user);
+  res.cookie('access_token', access_token, { httpOnly: true });
+  res.status(200).json({ message: "Logged in!" });
 });
 
-// Jika endpoint selain /auth diakses tanpa auth token maka akan throw error 401
-server.use(/^(?!\/auth).*$/, (req, res, next) => {
-  if (
-    req.headers.authorization === undefined ||
-    req.headers.authorization.split(" ")[0] !== "Bearer"
-  ) {
-    const status = 401;
-    const message = "Authorization header is missing";
-    res.status(status).json({ status, message });
-    return;
-  }
+// Logout endpoint untuk menghapus token cookie
+server.get("/auth/logout", (req, res) => {
+  return res
+    .clearCookie("access_token")
+    .status(200)
+    .json({ message: "Logged out!" });
+});
+
+// Endpoint untuk mendapatkan data user login dari auth
+server.get("/current/user", isAuthenticated, (req, res) => {
+  const token = req.cookies.access_token;
   try {
-    verifyToken(req.headers.authorization.split(" ")[1]);
-    next();
+    const data = verifyToken(token);
+    res.status(200).json({ data })
   } catch (err) {
     const status = 401;
     const message = "Error: token is invalid";
@@ -84,7 +107,7 @@ server.use(/^(?!\/auth).*$/, (req, res, next) => {
   }
 });
 
-server.use("/api", router);
+server.use("/api", isAuthenticated, router);
 server.listen(PORT, () => {
   console.log("JSON Server is running at", `http://localhost:${PORT}`);
 });
